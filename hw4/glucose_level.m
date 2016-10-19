93bfd135cd1869ee540da877b6d1888daeb5b81d
gddmkr42171822 <rwghost98@gmail.com>
2016-10-19 15:37:59 -0600

    made more progress on 2

--- hw4/glucose_level.m
+++ hw4/glucose_level.m
@@ -1,5 +1,4 @@
 function a = glucose_level(t, G, u)
-t = t + 1;
-a = [G(t) G(t-1) G(t-2) G(t-3) G(t-4) G(t-5) G(t-6) G(t-7) G(t-8) G(t-9) G(t-10)];
-a = horzcat(a, [u(t) u(t-1) u(t-2) u(t-3) u(t-4) u(t-5)]);
+a = [G(t-1) G(t-2) G(t-3) G(t-4) G(t-5) G(t-6) G(t-7) G(t-7=8) G(t-9) G(t-10) G(t-11)];
+a = horzcat(a, [u(t-1) u(t-2) u(t-3) u(t-4) u(t-5) u(t-6)]);
 end
--- hw4/p2.m
+++ hw4/p2.m
@@ -1,3 +1,7 @@
+
+options = optimoptions('linprog', 'Algorithm', 'dual-simplex');
+
+% Read in the csv file skipping the first row
 M = csvread('insulinGlucose1.csv', 1);
 
 % Insulin input values
@@ -5,32 +9,66 @@ u = M(:,1);
 % Glucose levels
 G = M(:,2);
 
-X = [];
+A = [];
+b = [];
+
+[m,~] = size(G(12:end));
+for i = 1:m
+    b = [b, G(i)];
+    b = [b, -G(i)];
+end
+
+b = b';
+
+[m,~] = size(u);
+
+% Initialize the objective function which is the minimum of the sum
+% of the absolute values of the residuals
+f = ones(1,17);
+
+% Create the constraint matrix A which is the residuals 
+% of glucose levels 10 -> 707.
+% If u = |x| then u = x and u = -x
+for t = 12:m
+    a = [G(t-1) G(t-2) G(t-3) G(t-4) G(t-5) G(t-6) G(t-7) G(t-8) G(t-9) G(t-10) G(t-11)];
+    a = horzcat(a, [u(t-1) u(t-2) u(t-3) u(t-4) u(t-5) u(t-6)]);
+    % Append the constraint ax.
+    A = [A;a];
+    
+    % Append the constraint -ax.
+    a = times(a, -1);
+    A = [A;a];
+end
+
+x = linprog(f,A,b);
+
+% Create an array of the time steps.
 t = [];
-
-[m,] = size(u);
-
-% Create the matrix A which in linear regression
-% is the matrix X.
-for c = 10:(m-1)
-    x = glucose_level(c, G, u);
-    X = [X;x];
+for i = 12:m
+    t = [t, i];
 end
 
-Y = G(10:end-1);
-% Linear Regression with least-squares
-% Find the coefficients
-B = X\Y;
-
-% Create an array of the possible history lengths
-for c = 10:(m-1)
-    t = [t, c];
+% Recreate the matrix A without the additional absolute value constraints
+A = [];
+for t = 12:m
+    a = [G(t-1) G(t-2) G(t-3) G(t-4) G(t-5) G(t-6) G(t-7) G(t-8) G(t-9) G(t-10) G(t-11)];
+    a = horzcat(a, [u(t-1) u(t-2) u(t-3) u(t-4) u(t-5) u(t-6)]);
+    A = [A;a];
 end
 
-% Plot the actual glucose levels at each history step t_i
-scatter(t,Y);
+t = [];
+for i = 12:m
+    t = [t, i];
+end
+
+% Plot the actual glucose levels at each history step t_i.
+scatter(t,G(12:end));
 hold on;
-% Plot the predicted glucose levels at each history step t_i
-% B stores the estimated coefficients of the best fit line
-plot(t, (X*B));
+
+% Plot the best fit line of the glucose levels at each history step t_i.
+% x stores the estimated coefficients of the best fit line.
+% |x| = sqrt(x^2)
+A = (A*x).^2;
+A = sqrt(A);
+plot(t, A);
 grid on;
