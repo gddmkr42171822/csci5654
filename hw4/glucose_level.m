b20b1334a35edbd082654673713bfa147b142037
gddmkr42171822 <rwghost98@gmail.com>
2016-10-20 21:42:02 -0600

    almost done with hw4 p2

--- hw4/glucose_level.m
+++
@@ -1,4 +0,0 @@
-function a = glucose_level(t, G, u)
-a = [G(t-1) G(t-2) G(t-3) G(t-4) G(t-5) G(t-6) G(t-7) G(t-7=8) G(t-9) G(t-10) G(t-11)];
-a = horzcat(a, [u(t-1) u(t-2) u(t-3) u(t-4) u(t-5) u(t-6)]);
-end
--- hw4/p2.m
+++ hw4/p2.m
@@ -20,18 +20,18 @@ end
 
 b = b';
 
-[m,~] = size(u);
-
 % Initialize the objective function which is the minimum of the sum
-% of the absolute values of the residuals
+% of the absolute values of the residuals.
+% Residuals have the form y - Ax.
 f = ones(1,17);
 
 % Create the constraint matrix A which is the residuals 
-% of glucose levels 10 -> 707.
+% of glucose levels 12 -> 707.
 % If u = |x| then u = x and u = -x
-for t = 12:m
-    a = [G(t-1) G(t-2) G(t-3) G(t-4) G(t-5) G(t-6) G(t-7) G(t-8) G(t-9) G(t-10) G(t-11)];
-    a = horzcat(a, [u(t-1) u(t-2) u(t-3) u(t-4) u(t-5) u(t-6)]);
+[m,~] = size(G);
+for t = 11:(m-1)
+    a = [G(t) G(t-1) G(t-2) G(t-3) G(t-4) G(t-5) G(t-6) G(t-7) G(t-8) G(t-9) G(t-10)];
+    a = horzcat(a, [u(t) u(t-1) u(t-2) u(t-3) u(t-4) u(t-5)]);
     % Append the constraint ax.
     A = [A;a];
     
@@ -40,35 +40,59 @@ for t = 12:m
     A = [A;a];
 end
 
+% Solve for the coefficents.
+% x will have an array of the coefficient values when it is solved.
 x = linprog(f,A,b);
 
-% Create an array of the time steps.
-t = [];
-for i = 12:m
-    t = [t, i];
-end
-
-% Recreate the matrix A without the additional absolute value constraints
+% Recreate the matrix A without the additional absolute value constraints.
 A = [];
-for t = 12:m
-    a = [G(t-1) G(t-2) G(t-3) G(t-4) G(t-5) G(t-6) G(t-7) G(t-8) G(t-9) G(t-10) G(t-11)];
-    a = horzcat(a, [u(t-1) u(t-2) u(t-3) u(t-4) u(t-5) u(t-6)]);
+for t = 11:(m-1)
+    a = [G(t) G(t-1) G(t-2) G(t-3) G(t-4) G(t-5) G(t-6) G(t-7) G(t-8) G(t-9) G(t-10)];
+    a = horzcat(a, [u(t) u(t-1) u(t-2) u(t-3) u(t-4) u(t-5)]);
     A = [A;a];
 end
 
+% Create an array of the history steps.
 t = [];
 for i = 12:m
     t = [t, i];
 end
 
-% Plot the actual glucose levels at each history step t_i.
+% Plot the true glucose levels at each history step t_i.
+figure(1);
 scatter(t,G(12:end));
 hold on;
 
-% Plot the best fit line of the glucose levels at each history step t_i.
+% Plot the best fit line of the predicted glucose levels at each 
+% history step t_i.
 % x stores the estimated coefficients of the best fit line.
 % |x| = sqrt(x^2)
 A = (A*x).^2;
 A = sqrt(A);
 plot(t, A);
 grid on;
+legend('True glucose levels', 'Best fit line of predicted glucose levels');
+title('True glucose levels and best fit line of predicted glucose levels vs history step');
+xlabel('History step');
+ylabel('Glucose level');
+saveas(1, 'predictions.png');
+
+% Plot the residuals.
+figure(2);
+residuals = [];
+trueGlucoseValues = G(12:end);
+predictedGlucoseValues = A;
+[m,~] = size(predictedGlucoseValues);
+for i = 1:m
+    residuals = [residuals, trueGlucoseValues(i) - predictedGlucoseValues(i)];
+end
+
+scatter(t, residuals);
+hold on;
+plot(t, zeros(m));
+grid on;
+title('Residuals vs history step');
+xlabel('History step');
+ylabel('Residual value');
+saveas(2, 'residuals.png');
+
